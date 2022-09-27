b2cff7e019bef5e6b1a3a493253df850add22a33
elaine <elainez13@nycstudents.net>
2022-09-27 13:03:32 -0400

    project done 1st draft

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -12,18 +12,43 @@ public class TipCalculator {
         System.out.print("How many people are in your group: " );
         int people = scan.nextInt();
         scan.nextLine();
-        System.out.print("What is the tip percentage: ");
+        System.out.print("What is the tip percentage (0-100): ");
         int tipPercentage = scan.nextInt();
         scan.nextLine();
         //dish info
         System.out.print("Enter the dish price, e.g. 10.50 (-1 to end): ");
         double dish = scan.nextDouble();
         scan.nextLine();
-        while (dish != -1)
+        double dishTotal = dish;
+        while (dish != -1)  //while loop
         {
             System.out.print("Enter the dish price, e.g. 10.50 (-1 to end): ");
             dish = scan.nextDouble();
+            dishTotal += dish;
         }
+        dishTotal ++;
+        //total tip
+        double totalTip = (tipPercentage*.01) * dishTotal;
+        String formattedtotalTip = formatter.format(totalTip);
+        //total bill
+        double totalBill = totalTip + dishTotal;
+        String formattedtotalBill = formatter.format(totalBill);
+        //per person cost before tip
+        double personBeforeTip = dishTotal/people;
+        String formattedpersonBeforeTip = formatter.format(personBeforeTip);
+        //tip per person
+        double tipPerPerson = totalTip/people;
+        String formattedtipPerPerson = formatter.format(tipPerPerson);
+        //total cost per person
+        double costPerPerson = personBeforeTip + tipPerPerson;
+        String formattedcostPerPerson = formatter.format(costPerPerson);
+        //finals
+        System.out.println("Your total bill before tip is: $"+ dishTotal);
+        System.out.println("Your total tip is: $"+ formattedtotalTip);
+        System.out.println("Your total bill after tip is: $"+ formattedtotalBill);
+        System.out.println("Per person cost before tip is: $"+ formattedpersonBeforeTip);
+        System.out.println("Per person cost before tip is: $"+ formattedtipPerPerson);
+        System.out.println("Total cost per person: " + formattedcostPerPerson);
     }
 
 
@@ -33,12 +58,12 @@ public class TipCalculator {
 
 //        System.out.print("Enter an EVEN integer: ");
 //        int number = scan.nextInt();
-
-        // the while loop begins and repeats as
-        // long as the condition is TRUE; as soon
-        // as the condition becomes FALSE, it stops
+//
+//         the while loop begins and repeats as
+//         long as the condition is TRUE; as soon
+//         as the condition becomes FALSE, it stops
 //        while (number % 2 != 0) {
-//            System.out.print("No silly, i said EVEN not odd! Try again: ");
+//            System.out.print("No silly, I said EVEN not odd! Try again: ");
 //            number = scan.nextInt();
 }
 
