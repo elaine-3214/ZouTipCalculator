27c72b23d6b00fd0adb0a595dc898f63f9598429
elaine <elainez13@nycstudents.net>
2022-09-27 18:09:28 -0400

    project done 3rd draft

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -52,19 +52,7 @@ public class TipCalculator {
     }
 
 
-//        double num = 12.458172109;
-//        String formattedNum = formatter.format(num);
-//        System.out.println(formattedNum);
 
-//        System.out.print("Enter an EVEN integer: ");
-//        int number = scan.nextInt();
-//
-//         the while loop begins and repeats as
-//         long as the condition is TRUE; as soon
-//         as the condition becomes FALSE, it stops
-//        while (number % 2 != 0) {
-//            System.out.print("No silly, I said EVEN not odd! Try again: ");
-//            number = scan.nextInt();
 }
 
 
